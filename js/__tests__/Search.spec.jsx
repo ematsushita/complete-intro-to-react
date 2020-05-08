8491de39f0256961463badfd8a09e3ee0358e6c7
ematsushita <elissa.matsushita@gmail.com>
2020-05-08 13:36:57 -0700

    set up testing

---
+++ js/__tests__/Search.spec.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import preload from '../../data.json';
+import Search from '../Search';
+import ShowCard from '../ShowCard';
+
+test('Search renders correctly', () => {
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
+});
+
+test('Search should render correct number of shows', () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+})
+
+test('Search should render correct number of shows based on search term', () => {
+  const component = shallow(<Search />);
+  const searchWord = 'black';
+  component.find('input').simulate('change', {target: {value: searchWord}})
+  const showCount = preload.shows
+                    .filter(show => `${show.title} ${show.description}`
+                    .toUpperCase().indexOf(searchWord.toUpperCase()) >= 0)
+                    .length
+  expect(component.find(ShowCard).length).toEqual(showCount)
+});
